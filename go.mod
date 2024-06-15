acf29978a2350e942dd72ee7eb64723bdb190e16
TrshPuppy <trshpuppy@proton.me>
2024-06-15 15:20:53 -0400

    first commit, gonna change name to sickem

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/trshpuppy/sickem
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// Take command line args
+
+	// start go routines?
+
+	// Each go routine creates a file (but lets start with CLI output)
+
+}
